2c060a4e10c7a42088f0d74d63cf0ea779f09ae9
Manik615 <110080946+Manik615@users.noreply.github.com>
2022-08-25 19:20:29 +0530

    EfCore CodeFirstApproach created

---
+++ EFCore/Entities/ActorDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Entities
+{
+    public class ActorDbContext : DbContext
+    {
+        public DbSet<Actor> Actors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server = (localdb)\MSSQLLocalDB; Database = Manik;"
+                                    +"Trusted_Connection = True"); 
+        }
+        
+    }
+}
---
+++ EFCore/Program.cs
@@ -0,0 +1,21 @@
+﻿using EFCore.Entities;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        using( var db = new ActorDbContext())
+        {
+            //db.Actors.AddRange(new Actor() {  AcademyWinner= false, Name="abc", Age = 24 },
+            //                   new Actor() {  AcademyWinner = true, Name = "xyz", Age = 29 },
+            //                   new Actor() {  AcademyWinner = true, Name = "suv", Age = 30 });
+            //var count = db.SaveChanges();
+            //Console.WriteLine($"{count} records saved");
+            foreach( var Actor in db.Actors)
+            {
+                Console.WriteLine($"Name: {Actor.Name} \t Id: {Actor.Id}\t Age: {Actor.Age}\t AcademyWinner: {Actor.AcademyWinner}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
